2c9591571b5ab18d4089f5f303d413147c5dfab4
ravioliroy <ravioliroymegazord@users.noreply.github.com>
2020-03-07 16:20:24 -0600

    Carousel Update

--- script.js
+++ script.js
@@ -12,23 +12,3 @@ function closeNav() {
 
 
 
-$(document).ready(function() { 
-  $("#owl-demo").owlCarousel({
-    navigation : true, // Show next and prev buttons
-      slideSpeed : 300,
-      paginationSpeed : 400,
-      singleItem:true
-     
-      // "singleItem:true" is a shortcut for:
-      // items : 1, 
-      // itemsDesktop : false,
-          // itemsDesktopSmall : false,
-          // itemsTablet: false,
-          // itemsMobile : false
-     
-      });
-     
-    });
-
-
-
